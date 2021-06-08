db0e6942549ab5006b40d1f4f47eb4325d21c3ec
rashmi <rashmiskp222@gmail.com>
2021-06-08 22:22:45 +0530

    page 1 is edited

--- GITPURPOSE/src/main/java/pages/page1.java
+++ GITPURPOSE/src/main/java/pages/page1.java
@@ -5,7 +5,7 @@ public class page1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("new page");
+		System.out.println("hello new page");
 
 	}
 
